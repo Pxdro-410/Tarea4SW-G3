5e88d53d9e9e71905cc109c3f2abc1f0d99d72ac
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-08-02 21:22:03 -0600

    feat: añadir funcion de grabacion en playwright para las pruebas

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'on', // graba video en cada prueba, sin importar el resultado
   },
 
   /* Configure projects for major browsers */
