c364f1c742b31c953cdf5dcd500b6b8d99f8f3a7
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-08-03 22:41:41 -0600

    feat: codigo de los test para el RF-3.2

---
+++ tests/rf-3-2-carrito-login.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.use({
+    ignoreHTTPSErrors: true
+});
+
+test('test-tc22', async ({ page }) => {
+    await page.goto('https://dev2.registro.gt/');
+    await page.getByRole('link', { name: 'Carrito' }).click();
+    await page.getByText('shopping_cart Tu carrito est').click();
+    await page.getByRole('link', { name: 'Buscar un dominio' }).click();
+});
+
+// test-tc-23 realizado de forma manual por el inicio de sesión
+
+// test-tc-24 realizado de forma manual por el inicio de sesión
